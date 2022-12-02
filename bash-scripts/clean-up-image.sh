4f224cb9c807598a5bc0dbc0e2eb469adfd9f27a
Wisnu Setiawan <wissnusetiawan@gmail.com>
2022-12-02 22:47:12 +0700

    Update variable pipelines
    Signed-off-by: Wisnu Setiawan <wissnusetiawan@gmail.com>

--- bash-scripts/clean-up-image.sh
+++ bash-scripts/clean-up-image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-src_src_container_registry=$1
-src_src_repository_name=$2
+src_container_registry=$1
+src_repository_name=$2
 src_image=$3
 
-msg="\tUsage:\t$0 <container registry> <repository name> <image>\n"
+msg="\tUsage:\t$0 <src container registry> <src repository name> <src image>\n"
 
 if [ $# -ne 3 ]; then
     echo -e $msg
@@ -14,7 +14,7 @@ fi
 echo "Validating ACR list..."
 registry_list=$(az acr repository list --name $src_container_registry -o json)
 if [ -z "$registry_list" ]; then
-    echo -e "Error:\tEither registry name $src_container_registry.\n$msg"
+    echo -e "Error:\tEither src registry name $src_container_registry.\n$msg"
     exit -1
 fi
 echo "Show $registry_list info..."
@@ -22,7 +22,7 @@ echo "Show $registry_list info..."
 echo "Validating ACR tag..."
 registry_tags=$(az acr repository show-tags --name $src_container_registry --repository $src_repository_name --top 10 --orderby time_desc --detail --query '[].name' -o tsv)
 if [ -z "$registry_tags" ]; then
-    echo -e "Error:\tEither repository name $src_repository_name.\n$msg"
+    echo -e "Error:\tEither src repository name $src_repository_name.\n$msg"
     exit -1
 fi
 echo "Show $registry_tags info..."
