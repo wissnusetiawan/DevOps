6eef04b132c9beb0285ee4a9379a00566ced7685
Wisnu Setiawan <wissnusetiawan@gmail.com>
2022-12-03 00:40:46 +0700

    Update variable pipelines
    Signed-off-by: Wisnu Setiawan <wissnusetiawan@gmail.com>

--- bash-scripts/clean-up-image.sh
+++ bash-scripts/clean-up-image.sh
@@ -11,17 +11,23 @@ if [ $# -ne 3 ]; then
     exit -1
 fi
 
-echo "Validating ACR list..."
-registry_list=$(az acr repository list --query "[?name=='$src_container_registry'].{name:name, group:resourceGroup}" -o json | jq -r '.[0]')
-if [ -z "$registry_list" ]; then
-    echo -e "Error:\tEither src registry name $src_container_registry.\n$msg"
-    exit -1
-fi
-# echo "Show $registry_list info..."
-src_resource_group=$(echo $registry_list | jq -r '.group')
+
+# Fetch the list of repositories
+registry_list=()
+registry_list="$(az acr repository list -n "$src_container_registry" -o json)"
+
+
+
+# echo "Validating ACR list..."
+# registry_list=$(az acr repository list --name $src_container_registry -o json)
+# if [ -z "$registry_list" ]; then
+#     echo -e "Error:\tEither src registry name $src_container_registry.\n$msg"
+#     exit -1
+# fi
+echo "Show $registry_list info..."
 
 echo "Validating ACR tag..."
-registry_tags=$(az acr repository show-tags --name $src_container_registry --repository $src_repository_name --top 10 --orderby time_desc --detail --query '[].name' -o tsv)
+registry_tags=$(az acr repository show-tags --name $src_container_registry --repository $src_repository_name --top 100 --orderby time_desc --detail --query '[].name' -o tsv)
 if [ -z "$registry_tags" ]; then
     echo -e "Error:\tEither src repository name $src_repository_name.\n$msg"
     exit -1
@@ -29,13 +35,15 @@ fi
 echo "Show $registry_tags info..."
 
 
-show_manifests=$(
-    az acr repository show-manifests --name "$src_container_registry" --repository "$src_repository_name" --top 100 --query '[].tags' \
-    --output tsv 
-    )
-echo "Show $show_manifests info..."
+# show_manifests=$(
+#     az acr repository show-manifests --name "$src_container_registry" --repository "$src_repository_name" --top 100 --query '[].tags' \
+#     --output tsv 
+#     )
+# echo "Show $show_manifests info..."
 
 
 echo "ACR delete image info..."
-az acr repository show-manifests --name "$src_container_registry" --repository "$src_repository_name" --orderby time_desc -o tsv --query '[].digest' | sed -n '100,$ p' | xargs -I% az acr repository delete --name "$src_container_registry" --image $src_image@% --yes
+az acr repository show-manifests --name "$src_container_registry" --repository "$src_repository_name" \
+         --orderby time_desc -o tsv --query '[].digest' | sed -n '100,$ p' | xargs -I% az acr repository delete \
+         --name "$src_container_registry" --image $src_image@% --yes
 
