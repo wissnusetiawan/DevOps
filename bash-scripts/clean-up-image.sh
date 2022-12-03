62651de259560e606b0fd62abfd23a0d1676acd1
Wisnu Setiawan <wissnusetiawan@gmail.com>
2022-12-03 12:56:32 +0700

    Update bash script
    Signed-off-by: Wisnu Setiawan <wissnusetiawan@gmail.com>

--- bash-scripts/clean-up-image.sh
+++ bash-scripts/clean-up-image.sh
@@ -30,35 +30,6 @@ else
             echo "Show $registry_list info..."
 
 
-    # # Search for keep images than 100 in each repository
-    # echo "################################################"
-    # echo "       EXECUTION OF KEEP IMAGES DELETION"
-    # echo "################################################"
-
-
-    # keep_image=()
-    # echo "${registry_list[@]}" | while read -r rep; do
-    #     keep_image=$(
-    #         az acr repository show-manifests --name "$container_registry" --repository "$rep" \
-    #             --query "[?tags[0]==null].digest" \
-    #             --orderby time_desc \
-    #             --output tsv 
-    #  )
-    #     if [ -z "${keep_image[@]}" ]; then
-    #         echo "INFO: Deleting image with keep 100 from image: $rep"
-    #     else
-    #         # Keep 100 images
-    #         echo
-    #         echo "${keep_image[@]}" | while read -r img; do
-    #             echo "WARN: Deleting image with keep 100 from image: $rep@$img"
-    #             az acr repository show-manifests --name "$container_registry" --repository "$rep" \
-    #                 | sed -n '100,$ p' | xargs -I% az acr repository delete \
-    #                 --name "$container_registry" --image $rep@$img% --yes
-    #         done
-    #     fi
-    # done
-
-
     # Search for untagged (dangling) images in each repository
     echo "################################################"
     echo "EXECUTION OF UNTAGGED (DANGLING) IMAGES DELETION"
@@ -101,7 +72,7 @@ else
                 --output tsv
         )
         if [ -z "${old_image[@]}" ]; then
-            echo "INFO: No images older than 30 days found in the repository: $rep"
+            echo "INFO: No images older than 30 days & keep 100 images found in the repository: $rep"
         else
             # Get how many images exist in the repository
             manifest_count=$(
