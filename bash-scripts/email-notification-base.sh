75c5a86fe07923e2cb4d528309a22ca669333748
Wisnu Setiawan <wissnusetiawan@gmail.com>
2022-12-10 02:05:11 +0700

    Update bash script
    Signed-off-by: Wisnu Setiawan <wissnusetiawan@gmail.com>

---
+++ bash-scripts/email-notification-base.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Stop execution on any error
+set -e
+
+    # Declare variables
+    SENDER=$1
+    RECIPIENT=$2
+    API_KEY=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
+
+    function usage() {
+        echo "ERROR: Missing or invalid arguments!"
+        echo "Usage: ${0} URL SENDER RECIPIENT PORT (OPTIONAL)"
+        exit 1
+    }
+
+    function send_email() {
+        local EMAIL_API="https://api.sendgrid.com/v3/mail/send"
+
+        local SUBJECT="KeyVault secret ${SECRET} about to expire"
+
+        local MESSAGE="<p> Dear Site Reliability Engineer, </p> \
+            <p> This is to notify you that the Key Vault secret <b>${SECRET}</b> will expire on <b>${SECRET_EXPIRY_DATE_SHORT}</b>. </p> \
+            <p> Please ensure the secret is rotated in a timely fashion. There are ${DATE_DIFF} days remaining. </p> \
+            <p> Sincerely yours, <br>DevOps Enterprise </p>"
+
+        local REQUEST_DATA='{
+            "personalizations": [
+                {
+                    "to": [{"email": "'${RECIPIENT}'"}],
+                    "dynamic_template_data": { "first_name": "Operations" }
+                }
+            ],
+            "from": {"email": "'${SENDER}'"},
+            "subject":"'${SUBJECT}'",
+            "content": [{"type": "text/html", "value": "'${MESSAGE}'"}]
+        }'
+
+        echo "INFO: Sending out notification via e-mail"
+        local CURL_HTTP_CODE
+        CURL_HTTP_CODE=$(
+            curl \
+                --request POST \
+                --url "${EMAIL_API}" \
+                --header "Authorization: Bearer ${API_KEY}" \
+                --header "Content-Type: application/json" \
+                --data "${REQUEST_DATA}" \
+                --output /dev/null \
+                --write-out "%{http_code}" \
+                --silent
+        )
+
+        if [[ "${CURL_HTTP_CODE}" -lt 200 || "${CURL_HTTP_CODE}" -gt 299 ]]; then
+            echo "ERROR: Failed sending notification with error code ${CURL_HTTP_CODE}!"
+            exit 1
+        fi
+    }
--- bash-scripts/email-notification.sh
+++ bash-scripts/email-notification.sh
@@ -3,50 +3,44 @@
 # Stop execution on any error
 set -e
 
-    # Declare variables
-    URL=$1
-    SENDER=$2
-    RECIPIENT=$3
+# Declare variables
+URL=$1
+SENDER=$2
+RECIPIENT=$3
 
 
-    function usage() {
-        echo "ERROR: Missing or invalid arguments!"
-        echo "Usage: ${0} URL SENDER RECIPIENT PORT (OPTIONAL)"
-        exit 1
-    }
+function usage() {
+    echo "ERROR: Missing or invalid arguments!"
+    echo "Usage: ${0} URL SENDER RECIPIENT PORT (OPTIONAL)"
+    exit 1
+}
 
-    function send_email() {
 
-        echo "INFO: Sending out notification via e-mail"
-        local CURL_HTTP_CODE
-        CURL_HTTP_CODE=$(
-            curl -s \
-                --url "$URL" \
-                --mail-from "$SENDER" \
-                --mail-rcpt "$RECIPIENT" \
-                --upload-file - 
+function send_email() {
 
-                <<EOF
-                From: Magic Elves <wissnusetiawan@gmail.com>
-                To: Wisnu Inbox <wisnu@clade.ventures>
-                Subject: Publish Result Unit Test
-                Content-Type: multipart/alternative; boundary="boundary-string"
+    local result=$( cat /home/vsts/work/_temp/cchtml/index.html )
 
-                --boundary-string
-                Content-Type: text/plain; charset="utf-8"
-                Content-Transfer-Encoding: quoted-printable
-                Content-Disposition: inline
+    echo "INFO: Sending out notification via e-mail"
+    curl \
+    --url "$URL" \
+    --mail-from "$SENDER" \
+    --mail-rcpt "$RECIPIENT" \
+    --upload-file - << EOF
+From: "Magic" <$SENDER>
+To: "Wisnu" <$RECIPIENT>
+Subject: Publish Result Unit Test
+Content-Type: multipart/alternative; boundary="boundary-string"
 
-                Congrats for sending test email!
+--boundary-string
+Content-Type: text/html; charset="utf-8"
+Content-Transfer-Encoding: quoted-printable
+Content-Disposition: inline
 
-                Inspect it us
+$result
 
-                Good luck! Hope it works.
-                --boundary-string--
+--boundary-string--
 EOF
-        )
 
-
-    }
+}
     
-    send_email
+send_email
